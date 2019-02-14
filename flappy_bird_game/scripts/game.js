84402f74e90d8ad029a17f07f1cce1a308aea19a
TarrinRos <tarindis@gmail.com>
2019-02-14 19:42:52 +0200

    Draw images on canvas for game

--- flappy_bird_game/scripts/game.js
+++ flappy_bird_game/scripts/game.js
@@ -10,13 +10,25 @@ const pipe_bottom = new Image();
 // images sources
 bird.src = 'images/flappy_bird_bird.png';
 backgr.src = 'images/flappy_bird_bg.png';
-foregr.src = 'images/flappy_bird_fgpng';
+foregr.src = 'images/flappy_bird_fg.png';
 pipe_top.src = 'images/flappy_bird_pipe_top.png';
 pipe_bottom.src = 'images/flappy_bird_pipe_bottom.png';
 
-// functions
+// gap between top and bottom pipes
+const gap = 90;
 
+//
+
+// functions
 function draw() {
     // draws image on the canvas (img variable , pos x, pos y)
     context.drawImage(backgr, 0, 0);
+    context.drawImage(foregr, 0, canv.height - foregr.height);
+    context.drawImage(pipe_top, 100, 0);
+    context.drawImage(pipe_bottom, 100, 0 + pipe_top.height + gap);
+    context.drawImage(pipe_top, 100, 0);
+    context.drawImage(bird, 10, 150);
 }
+
+// Calls 'draw' function when pipe_bottom loads
+pipe_bottom.onload = draw();
