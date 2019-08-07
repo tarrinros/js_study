92fd5b1b85c39302826a18e3078a61ba43d198a9
tarrinros <tarindis@gmail.com>
2019-08-07 16:11:45 +0300

    React todo list init

---
+++ react-youtube/src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react';
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
+
+function App() {
+  const [todos, setTodos] = React.useState([
+      {id: 1, completed: false, title: 'Купить хлеб'},
+      {id: 2, completed: false, title: 'Купить масло'},
+      {id: 3, completed: false, title: 'Купить молоко'}
+    ]
+  );
+
+  useEffect(() => {
+
+  }, []);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([
+      {
+        title: title,
+        id: Date.now(),
+        completed: false
+      }
+    ]))
+  }
+
+  return (
+    <Context.Provider value={{removeTodo}}>
+      <div className='wrapper'>
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo}></AddTodo>
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <p> No todos </p>}
+
+      </div>
+    </Context.Provider>
+  );
+}
+
+export default App;
