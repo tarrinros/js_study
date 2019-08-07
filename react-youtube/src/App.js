c04832df7896c9b2ffa65412a33b70eadec205f5
tarrinros <tarindis@gmail.com>
2019-08-07 16:18:18 +0300

    Add fetching fake JSON object from server

--- react-youtube/src/App.js
+++ react-youtube/src/App.js
@@ -5,14 +5,18 @@ import AddTodo from "./Todo/AddTodo";
 
 function App() {
   const [todos, setTodos] = React.useState([
-      {id: 1, completed: false, title: 'Купить хлеб'},
-      {id: 2, completed: false, title: 'Купить масло'},
-      {id: 3, completed: false, title: 'Купить молоко'}
+      // {id: 1, completed: false, title: 'Купить хлеб'},
+      // {id: 2, completed: false, title: 'Купить масло'},
+      // {id: 3, completed: false, title: 'Купить молоко'}
     ]
   );
 
   useEffect(() => {
-
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTodos(todos)
+    })
   }, []);
 
   function toggleTodo(id) {
