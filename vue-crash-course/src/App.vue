ff986f034cec60889b6f641de3649894f361a12a
tarrinros <tarindis@gmail.com>
2019-08-07 20:08:30 +0300

    Add Todo adding to the TodoList

--- vue-crash-course/src/App.vue
+++ vue-crash-course/src/App.vue
@@ -1,43 +1,59 @@
 <template>
   <div id="app">
     <h1>Todo application</h1>
+    <AddTodo
+      @add-todo="addTodo"
+    />
     <hr>
     <TodoList v-bind:todos="todos"
-    @remove-todo="removeTodo"/>
+              @remove-todo="removeTodo"/>
   </div>
 </template>
 
 <script>
-import TodoList from '@/components/TodoList'
-export default {
-  name: 'app',
-  data() {
-    return {
-      todos: [
-        {id: 1, title: 'Купить хлеб', completed: false},
-        {id: 2, title: 'Купить масло', completed: false},
-        {id: 3, title: 'Купить молоко', completed: false}
-      ]
-    }
-  },
-  components: {
-    TodoList: TodoList
-  },
-  methods: {
-    removeTodo(id) {
-      this.todos = this.todos.filter(t => t.id !== id)
+  import TodoList from '@/components/TodoList'
+  import AddTodo from '@/components/AddTodo'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        todos: [
+          // {id: 1, title: 'Купить хлеб', completed: false},
+          // {id: 2, title: 'Купить масло', completed: false},
+          // {id: 3, title: 'Купить молоко', completed: false}
+        ]
+      }
+    },
+    mounted() {
+      fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+        .then(response => response.json())
+        .then(json => {
+          this.todos = json
+        })
+    },
+    components: {
+      TodoList,
+      AddTodo
+    },
+    methods: {
+      removeTodo(id) {
+        this.todos = this.todos.filter(t => t.id !== id)
+      },
+      addTodo(todo) {
+        this.todos.push(todo)
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- vue-crash-course/src/components/TodoItem.vue
+++ vue-crash-course/src/components/TodoItem.vue
@@ -3,7 +3,7 @@
     <li>
       <span v-bind:class="{done: todo.completed}">
         <input type="checkbox" v-on:change="todo.completed = !todo.completed">
-        <strong>{{ todo.id }}</strong>
+        <strong>{{ index + 1 }}</strong>
         {{ todo.title }}
       </span>
       <button class="rm" v-on:click="$emit('remove-todo', todo.id)">&times;</button>
@@ -17,7 +17,8 @@
       todo: {
         type: Object,
         required: true
-      }
+      },
+      index: Number
     }
   }
 </script>
