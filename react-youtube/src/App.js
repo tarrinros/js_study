c14dc2e5cd5cb812581bc28c8a51e3116d9a603b
tarrinros <tarindis@gmail.com>
2019-08-07 17:17:27 +0300

    Add Modal window

--- react-youtube/src/App.js
+++ react-youtube/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import Loader from "./Loader";
+import Modal from "./Modal/Modal";
 
 const AddTodo = React.lazy(() =>
   new Promise(resolve => {
@@ -59,6 +60,7 @@ function App() {
   return (
     <Context.Provider value={{removeTodo}}>
       <div className='wrapper'>
+        <Modal />
         <h1>React tutorial</h1>
         <React.Suspense fallback={<p>Loading...</p>}>
           <AddTodo onCreate={addTodo} />
--- react-youtube/src/Loader.js
+++ react-youtube/src/Loader.js
@@ -1,9 +1,5 @@
 import React from 'react'
 
-function Loader() {
-
-}
-
 export default () => <div style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}>
   <div className="lds-roller">
     <div/>
